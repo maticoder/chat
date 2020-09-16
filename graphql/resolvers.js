040e76ebe77c2c0e5650cce98c4604f9d32e24b3
Mati <mati@Mati-iMac-Pro.local>
2020-09-16 14:27:00 +0200

    added register mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,18 +1,71 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { UserInputError } = require("apollo-server");
+
 module.exports = {
     Query: {
-        getUsers: () => {
-            const users = [
-                {
-                    username: "john",
-                    email: "john@email.com",
-                },
-                {
-                    username: "jane",
-                    email: "jane@email.com",
-                },
-            ];
+        getUsers: async () => {
+            try {
+                const users = await User.find({});
+                return users;
+            } catch (err) {
+                console.log(err);
+            }
+        },
+    },
+    Mutation: {
+        register: async (_, args) => {
+            let { username, email, password, confirmPassword } = args;
+            let errors = {};
 
-            return users;
+            try {
+                // Validate input data
+                if (email.trim() === "")
+                    errors.email = "email must not be empty";
+                if (username.trim() === "")
+                    errors.username = "username must not be empty";
+                if (password.trim() === "")
+                    errors.password = "password must not be empty";
+                if (confirmPassword.trim() === "")
+                    errors.confirmPassword =
+                        "confirm password must not be empty";
+
+                if (password !== confirmPassword)
+                    errors.confirmPassword = "passwords must match";
+
+                // Check if username / email exists
+                const userByUsername = await User.findOne({
+                    username,
+                });
+                const userByEmail = await User.findOne({
+                    email,
+                });
+
+                if (userByUsername) errors.username = "username is taken";
+                if (userByEmail) errors.email = "email is taken";
+
+                if (Object.keys(errors).length > 0) {
+                    throw errors;
+                }
+
+                // Hash password
+                password = await bcrypt.hash(password, 6);
+
+                // Create user
+                const user = await User.create({
+                    username,
+                    email,
+                    password,
+                });
+
+                // Return user
+                return user;
+            } catch (err) {
+                if (err.name === "ValidationError") {
+                    errors.general = "Validation problem";
+                }
+                throw new UserInputError("Bad input", { errors });
+            }
         },
     },
 };
--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const { UserInputError } = require("apollo-server");
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -5,11 +6,26 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        maxlength: 20,
     },
     email: {
         type: String,
         required: true,
         unique: true,
+        maxlength: 100,
+        validate: {
+            validator: (email) => {
+                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email);
+            },
+            message: "Please fill a valid email address",
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
     },
 });
 
